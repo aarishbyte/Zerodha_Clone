231f1b9e5d58219eca49a23fc9e2e5eb31217f15
Aarish Hasan <hasanarish5@gmail.com>
2026-07-25 01:13:40 +0530

    Start with the Home Page

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -3,7 +3,16 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>Hero</h1>
+        <div className='container p-5 mb-5'>
+            <div className="row text-center">
+                <img src='media/images/homeHero.png' alt='Hero Image' className='mb-5'></img>
+                <h1 className='mt-5'>Invest in everthing</h1>
+                <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+                <button className='p-2 btn btn-primary fs-5 mb-5' style={{width:"20%", margin:"0 auto"}}>SignUp Now</button>
+
+            </div>
+
+        </div>
      );
 }
 
